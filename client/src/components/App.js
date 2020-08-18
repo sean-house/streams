42e03364b94403d712a51c63fb267b519574929f
Sean House <33880135+sean-house@users.noreply.github.com>
2020-08-18 22:12:32 +0100

    End of Sect 20

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
+import Header from './Header';
 
 
 
 const App = () => {
     return (
-    <div>
+    <div className = "ui container">
         <BrowserRouter>
-            <Route path='/' exact component={StreamList} />
-            <Route path='/streams/show' component={StreamShow} />
-            <Route path='/streams/new' component={StreamCreate} />
-            <Route path='/streams/edit' component={StreamEdit} />
-            <Route path='/streams/delete' component={StreamDelete} />
+            <div>
+                <Header />
+                <Route path='/' exact component={StreamList} />
+                <Route path='/streams/show' component={StreamShow} />
+                <Route path='/streams/new' component={StreamCreate} />
+                <Route path='/streams/edit' component={StreamEdit} />
+                <Route path='/streams/delete' component={StreamDelete} />
+            </div>
         </BrowserRouter>
     </div>
     );
